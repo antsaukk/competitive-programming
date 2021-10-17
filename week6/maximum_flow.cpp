143db3358fbad1b885f1e91e1cdcacd04b7d6b92
Anton Saukkonen <63663359+antsaukk@users.noreply.github.com>
2021-10-17 20:54:36 +0300

    maximum flow working

--- week6/maximum_flow.cpp
+++ week6/maximum_flow.cpp
@@ -9,10 +9,9 @@
 using namespace std;
 
 i64 threshold = 0;
-i64 source = 0;
-int n, m;
-vector<vector<pair<i64, i64>>> graph;
-//vector<vector<pair<i64, i64>>> rGraph;
+size_t source = 0;
+size_t n, m;
+vector<i64*> graph;
 vector<int> visited;
 
 void falsify() {
@@ -24,40 +23,34 @@ void potentiate() {
 	threshold = pow(2, static_cast<i64>(floor(log(threshold)/log(2))));
 }
 
-int index(int ind) {
-	return ind - 1;
+size_t index(size_t ind) {
+	return ind-1;
 }
 
-bool scalingCondition(i64 capacity, bool visited) {
-	//return (capacity >= threshold && !visited); 
-	//return (capacity > threshold && !visited); 
-	return (capacity > threshold); 
+bool scalingCondition(i64 capacity) {
+	return (capacity >= threshold); 
 }
 
-i64 edge(i64 node, size_t ind) {
-	return graph[node][ind].first;
+i64 weight(size_t node, size_t edge) {
+	return graph[node][edge];
 }
 
-i64 weight(i64 node, size_t ind) {
-	return graph[node][ind].second;
+void augment(size_t node, size_t edge, i64 flow) {
+	graph[node][edge] = weight(node, edge) - flow;
+	graph[edge][node] = weight(edge, node) + flow;
 }
 
-void augment(i64 node, size_t ind, i64 flow) {
-	graph[node][ind] = make_pair(edge(node, ind), weight(node, ind) - flow);
-	graph[node][ind] = make_pair(edge(node, ind), weight(node, ind) - flow);
-}
-
-i64 DFS(i64 node, i64 flow) {
+i64 DFS(size_t node, i64 flow) {
 	if (node == index(n)) return flow;
 	if (visited[node]) return 0;
 	visited[node] = true;
 
-	for (size_t ind = 0; ind < graph[node].size(); ind++) {
-		auto cap = weight(node, ind);
-		if (scalingCondition(cap, visited[edge(node, ind)])){
-			i64 f = DFS(edge(node, ind), min(flow, cap));
+	for (size_t edge = 0; edge < n; edge++) {
+		auto cap = weight(node, edge);
+		if (scalingCondition(cap)){
+			i64 f = DFS(edge, min(flow, cap));
 			if (f > 0) {
-				augment(node, ind, f);
+				augment(node, edge, f);
 				return f;
 			}
 		}
@@ -65,44 +58,65 @@ i64 DFS(i64 node, i64 flow) {
 	return 0;
 }
 
+// Edmond's Karp search algorithm
+i64 solveEdmondKarp() {return 0;}
 
-i64 solve() {
+//with dfs that uses scaling solution
+i64 solveScaling() {
 	i64 maxflow = 0;
 	i64 flow = 0;
-	/*while(threshold > 0) {
+	while(threshold > 0) {
 		do {
-			falsify();
+			//falsify();
 			flow = DFS(source, INF);
 			maxflow+=flow;
 		} while(flow!=0);
 		threshold /= 2;
-	}*/
-	threshold = 0;
-	do {
-			falsify();
-			flow = DFS(source, INF);
-			maxflow+=flow;
-		} while(flow!=0);
+	}
 	return maxflow;
+}
+
+//naive dfs without scaling condition
+i64 solve() {
+	threshold = 0;
+	i64 maxflow = 0;
+	i64 flow = 0;
+	do {
+		falsify();
+		flow = DFS(source, INF);
+		maxflow+=flow;
+	} while(flow!=0);
+	return maxflow;
+}
+
+void allocate(){
+	graph.resize(n);
+	for(size_t i = 0; i < n; i++) {
+		graph[i] = new i64[n];
+	}
+}
+
+void freemem(){
+	for(size_t i = 0; i < n; i++) 
+		delete[] graph[i];
 }
 
 int main() {
 	cin >> n >> m; 
 	
 	//construct weighted directed graph and reversed graph
-	graph.resize(n);
-	//rGraph.resize(n);
+	allocate();
 	visited.resize(n);
-	for(int i = 0; i < m; i++) {
-		int a, b; i64 c; 
+	for(size_t i = 0; i < m; i++) {
+		size_t a, b; i64 c; 
 		cin >> a >> b >> c; 
-		graph[index(a)].push_back(make_pair(index(b), c));
-		rGraph[index(b)].push_back(make_pair(index(a), 0));
+		graph[index(a)][index(b)] = c;
+		graph[index(b)][index(a)] = 0;
 		threshold = max(c, threshold);
-		//threshold += c;
 	}
 	potentiate();
-	cout << solve();
+	cout << solveScaling();
+	freemem();
 
 	return 0;
 }
