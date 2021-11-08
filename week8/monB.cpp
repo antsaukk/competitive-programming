dac5003475b7c2b6823fbae4426c7a6ae9a4b72b
Anton Saukkonen <63663359+antsaukk@users.noreply.github.com>
2021-11-08 17:25:13 +0200

    monday contest solved problems

---
+++ week8/monB.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <assert.h>
+#include <cmath>
+
+// https://cses.fi/380/result/3098804/ - smallest period of a string
+
+using namespace std; 
+
+class Hash {
+public:
+	Hash() {}
+
+	void build_hash(const std::string& s) {
+		size_t n = s.size(); 
+		h.resize(n); 
+		p.resize(n);
+		hfy(s); 
+		pfy(n);
+	}
+
+	void hfy(const std::string& s) {
+		h[0] = s[0]; 
+		for(size_t k = 1; k < s.size(); k++) {
+			h[k] = (h[k-1] * A + s[k]) % B;
+		}
+	}
+
+	void pfy(size_t n) {
+		p[0] = 1; 
+		for(size_t k = 1; k < n; k++) {
+			p[k] = (p[k-1] * A) % B;
+		}
+	}
+
+	int get_hash(size_t a, size_t b) {
+		assert(b > a);
+		if (a > 0){
+			return (h[b] - h[a-1] * p[b-a+1]) % B;
+		} else return h[b];
+	}
+
+	void z_algorithm(std::string& s) {
+		int n = s.size();
+		z.resize(n); 
+		int x = 0;
+		int y = 0;
+		for (int i = 1; i < n; i++) {
+			z[i] = std::max(0, std::min(z[i-x], y-i+1));
+			while(i + z[i] < n && s[z[i]] == s[i + z[i]]){
+				x = i; 
+				y = i + z[i]; 
+				z[i]++;
+			}
+		}
+	}
+
+	std::vector<int>& z_array() {
+		return z;
+	}
+
+private:
+	std::vector<int> h;
+	std::vector<int> p;
+	std::vector<int> z;
+	static const int64_t A = 911382323; 
+	static const int64_t B = 972663749; 
+};
+
+
+void solve(string& s) {
+	Hash sol;
+	sol.z_algorithm(s);
+	auto z = sol.z_array();
+
+	size_t p = 0;
+	while (p + z[p] < s.size()) p++;
+
+	for(size_t i = 0; i < p; i++)
+		cout << s[i];
+}
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	string str;
+	cin >> str;
+
+	solve(str);  
+
+	return 0;
+}
