e0c247d0964523014b7fd068eeab569a3a7a7b08
Anton Saukkonen <63663359+antsaukk@users.noreply.github.com>
2022-04-04 17:01:03 +0300

    wip subarray distinct values

--- problem solving with algorithms course/subarr.cpp
+++ problem solving with algorithms course/subarr.cpp
@@ -17,21 +17,21 @@ RESULT:
 #include <iostream>
 #include <vector>
 #include <set>
+#include <map>
 #include <algorithm>
-#include <iterator>
 
 #define ui64 uint64_t
-
+ 
 using namespace std;
 
 ui64 subarrays = 0;
-
+ 
 template<typename T>
 void display(T val) {
-        cout << val << " ";
+        cout << val;
 }
-
-template <typename Container,
+ 
+/*template <typename Container,
           typename T>
 void read(Container& container, T size_)
 {
@@ -42,60 +42,119 @@ void read(Container& container, T size_)
                                  forward<ui64>(element));
                 display("there");
         }
+}*/
+ 
+template <typename T>
+void read(vector<T>& container, size_t size_)
+{
+        for(size_t i = 0; i < size_; i++) {
+                ui64 element;
+                cin >> element;
+                container[i] = element;
+        }
 }
-
+ 
 template <typename T,
           typename K>
-bool there_is_space_in_a_bucket(set<T>& bucket,
+bool there_is_space_in_a_bucket(map<T,K>& bucket,
                                 K limit)
 {
-        return bucket.size() < limit;
+        return bucket.size() <= limit;
 }
-
-void increase_subarrays() 
+ 
+void increase_number_of_subarrays(size_t s) 
 {
-        subarrays++;
+        subarrays+=s;
 }
-
-template <typename Container,
+ 
+/*template <typename Container,
           typename T>
 void solve(Container& container,
            T range)
 {
-        for(auto left = container.begin();
-                left < container.end(); 
-                next(left)) {
+        typedef typename Container::iterator iter;
+        for(iter left = container.begin();
+                left != container.end(); 
+                ++left) {
                 set<ui64> bucket;
-                auto right = left;
+                iter right = left;
+                display("here");
                 while(there_is_space_in_a_bucket(bucket, range) 
-                        && right < container.end())
+                        && right != container.end())
                 {
                         bucket.insert(*right);
                         next(right);
                 }
-                increase_subarrays();
+                increase_number_of_subarrays();
+        }
+}*/
+
+size_t upper_bound(size_t left, size_t range, size_t size_) {
+        return left + range < size_ ? left + range : size_;
+}
+ 
+template <typename T>
+void solve(vector<T>& container,
+                size_t size_,
+                size_t range)
+{
+        size_t right = upper_bound(0u, range, size_);
+        map<T,size_t> bucket;
+
+        for(size_t left = 0; left < right; left++) {
+                bucket[container[left]]++;
+        }
+
+        for(size_t left = 0; left < size_; left++) {
+                right = upper_bound(left, range, size_);
+                
+                increase_number_of_subarrays(right-left);
+
+                for (size_t counter = right; counter < size_; counter++) {
+                        bucket[container[counter]]++;
+
+                        if (there_is_space_in_a_bucket(bucket, range)) {
+                                increase_number_of_subarrays(1u);
+                        } else {
+                                right = counter;
+                                break;
+                        }
+                }
+                
+                if (right < size_) {
+                        if (bucket.at(container[left]) == 1) {
+                                bucket.erase(container[left]);
+                        } else{
+                                bucket.erase(container[right]);
+                                bucket[container[left]]--;
+                        }
+
+                } else {
+                        bucket[container[left]]--;
+                }
+                
+
         }
 }
-
-
+ 
+ 
 // untie std in/out to speed up computation
 void desyncio() {
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
 }
-
+ 
 int main() {
-        //desyncio();
-
+        desyncio();
+ 
         size_t n, k; 
         cin >> n >> k;
-
+ 
         vector<ui64> v(n);
         read(v, n);
-        display("here");
-        solve(v, k);
-
+        solve(v, n, k);
+ 
         display(subarrays);
-
+ 
         return 0;
 }
