313f78a51d1eb80600f36a7e3a0f7628d3393fc5
Anton Saukkonen <saukkonen.anton@gmail.com>
2022-10-10 11:37:27 +0300

    challenge apartments solved

--- cses/Sorting and Searching/apartments/apartments.cpp
+++ cses/Sorting and Searching/apartments/apartments.cpp
@@ -1,3 +1,40 @@
+/*
+There are n applicants and m free apartments. Your task is to distribute the apartments so that as many applicants as possible will get an apartment.
+
+Each applicant has a desired apartment size, and they will accept any apartment whose size is close enough to the desired size.
+ 
+INPUT:
+The first input line has three integers n, m, and k: the number of applicants, the number of apartments, and the maximum allowed difference.
+
+The next line contains n integers a1,a2,…,an: the desired apartment size of each applicant.
+If the desired size of an applicant is x, he or she will accept any apartment whose size is between x−k and x+k.
+
+The last line contains m integers b1,b2,…,bm: the size of each apartment.
+
+OUTPUT:
+
+Print one integer: the number of applicants who will get an apartment.
+ 
+CONSTRAINTS:
+1 <= n,m <= 2*10^5
+0 <= k <= 10^9
+1 <= ai,bi <= 10^9
+
+EXAMPLE:
+
+Input:
+4 3 5
+60 45 80 60
+30 60 75
+
+Output:
+2
+
+VERIFY HERE:
+
+https://cses.fi/problemset/task/1084/
+
+*/
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -32,7 +69,6 @@ void ReadStream(vector<T>& container) {
  
 template <typename T>
 void solve(const size_t n, const T k, priority_queue<T>& applicants, vector<T>& apartments) {
-	//sort(apartments.begin(), apartments.end(), greater<T>());
 	sort(apartments.begin(), apartments.end());
  
 	size_t matched = 0;
@@ -51,13 +87,7 @@ void solve(const size_t n, const T k, priority_queue<T>& applicants, vector<T>& 
  		PrintOut(")\n");*/
  
 		auto apartment_candidate = lower_bound(lower_price_ap, apartments.end(), applicant);
- 
-		/*if (apartment_candidate==lower_price_ap && (abs(applicant - *apartment_candidate) <= k)) {
-			matched++;
-			lower_price_ap++;
-			PrintOut
-			continue;
-		}*/
+
  
 		if (apartment_candidate < apartments.end()) {
  
@@ -66,7 +96,7 @@ void solve(const size_t n, const T k, priority_queue<T>& applicants, vector<T>& 
 			if ((abs(applicant - *lower_candidate) <= k)) {
 				matched++; 
  
-				while ((abs(applicant - *lower_candidate) <= k) && lower_candidate > lower_price_ap) { //here is the trouble
+				while ((abs(applicant - *lower_candidate) <= k) && lower_candidate > lower_price_ap) { 
 					lower_candidate--;
 				}
  
@@ -74,17 +104,19 @@ void solve(const size_t n, const T k, priority_queue<T>& applicants, vector<T>& 
 					lower_candidate++;
 				}
  
-				PrintOut("Appl and apart are matched 1: ");
+				/*PrintOut("Appl and apart are matched 1: ");
 				PrintOut(*(lower_candidate), " ");
-				PrintOut(applicant, "\n");
+				PrintOut(applicant, "\n");*/
  
 				lower_price_ap = lower_candidate + 1;
+
+				if (lower_price_ap == apartments.end()) break;
 				
 			} else if (lower_candidate != lower_price_ap && (abs(applicant - *(lower_candidate-1)) <= k)) {
 				matched++; 
 				lower_candidate--;
  
-				while ((abs(applicant - *lower_candidate) <= k) && lower_candidate > lower_price_ap) { //here is the trouble
+				while ((abs(applicant - *lower_candidate) <= k) && lower_candidate > lower_price_ap) {
 					lower_candidate--;
 				}
  
@@ -92,11 +124,13 @@ void solve(const size_t n, const T k, priority_queue<T>& applicants, vector<T>& 
 					lower_candidate++;
 				}
  
-				PrintOut("Appl and apart are matched 2: ");
+				/*PrintOut("Appl and apart are matched 2: ");
 				PrintOut(*(lower_candidate), " ");
-				PrintOut(applicant, "\n");
+				PrintOut(applicant, "\n");*/
  
 				lower_price_ap = lower_candidate + 1;
+
+				if (lower_price_ap == apartments.end()) break;
 			}
  
 		} else {
@@ -113,17 +147,19 @@ void solve(const size_t n, const T k, priority_queue<T>& applicants, vector<T>& 
 					lower_candidate++;
 				}
  
-				PrintOut("Appl and apart are matched 4: ");
+				/*PrintOut("Appl and apart are matched 4: ");
 				PrintOut(*(lower_candidate), " ");
-				PrintOut(applicant, "\n");
+				PrintOut(applicant, "\n");*/
  
 				lower_price_ap = lower_candidate + 1; // ?
+
+				if (lower_price_ap == apartments.end()) break;
  
 			} else if (abs(*lower_candidate - applicant) <= k) {
 					matched++;
-					PrintOut("Appl and apart are matched 5: ");
+					/*PrintOut("Appl and apart are matched 5: ");
 					PrintOut(*(lower_candidate), " ");
-					PrintOut(applicant, "\n");
+					PrintOut(applicant, "\n");*/
 					break;
 					
 			}
